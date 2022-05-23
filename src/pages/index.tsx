d5fd2a49e81b5f7de5580ea7075b163a52b50b4f
YvinisAlves <55796360+YvinisAlves@users.noreply.github.com>
2022-05-23 14:53:19 -0300

    commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,36 @@
+import Botao from "../components/Botao";
+import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
+
+export default function Home() {
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 45, '2'),
+    new Cliente('Carlos', 23, '3'),
+    new Cliente('Pedro', 54, '4'),
+  ]
+
+  function clienteSelecionado(cliente: Cliente) {
+
+  }
+  function clienteExcluido(cliente: Cliente) {
+
+  }
+
+  return (
+    <div className={`
+    flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white`}>
+      <Layout titulo="Cadastro Simples">
+        <div className="flex justify-end">
+          <Botao cor="green" className="mb-4">Novo Cliente</Botao>
+        </div>
+        <Tabela
+          clientes={clientes}
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido} />
+      </Layout>
+
+    </div>
+  )
+}
