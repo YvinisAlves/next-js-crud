d29309eddd41acb97d63598b8a2199e14f2377c4
YvinisAlves <55796360+YvinisAlves@users.noreply.github.com>
2022-05-23 23:12:07 -0300

    commit

--- src/core/Cliente.ts
+++ src/core/Cliente.ts
@@ -1,25 +1,25 @@
 export default class Cliente {
-     #id: string
-     #nome: string
-     #idade: number
+    #id: string
+    #nome: string
+    #idade: number
 
-    constructor(nome:string, idade:number, id:string = null){
+    constructor(nome: string, idade: number, id: string = null) {
         this.#nome = nome
         this.#idade = idade
         this.#id = id
     }
 
-    static vazio(){
-        return new Cliente('',0)
+    static vazio() {
+        return new Cliente('', 0)
     }
 
-    get id(){
+    get id() {
         return this.#id
     }
-    get nome(){
+    get nome() {
         return this.#nome
     }
-    get idade(){
+    get idade() {
         return this.#idade
     }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,67 @@
+import { useEffect, useState } from "react";
+import ColecaoCliente from "../backend/db/ColecaoCliente";
 import Botao from "../components/Botao";
+import Formulario from "../components/Formulario";
 import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
+import ClienteRepositorio from "../core/ClienteRepositorio";
 
 export default function Home() {
-  const clientes = [
-    new Cliente('Ana', 34, '1'),
-    new Cliente('Bia', 45, '2'),
-    new Cliente('Carlos', 23, '3'),
-    new Cliente('Pedro', 54, '4'),
-  ]
+  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+  const [clientes, setClientes] = useState<Cliente[]>([])
+  const repo: ClienteRepositorio = new ColecaoCliente()
+
+  useEffect(obterTodos, [])
+
+  function obterTodos(){
+
+    repo.obterTodos().then(clientes => {
+      setClientes(clientes)
+      setVisivel('tabela')
+    })
+  }
+
+  async function salvarCliente(cliente: Cliente){
+    await repo.salvar(cliente)
+    obterTodos()
+  }
+  function novoCliente(){
+    setCliente(Cliente.vazio())
+    setVisivel('form')
+  }
 
   function clienteSelecionado(cliente: Cliente) {
-
+    setCliente(cliente)
+    setVisivel('form')
   }
   function clienteExcluido(cliente: Cliente) {
-
+    console.log(`Excluir ${cliente.nome}`)
   }
 
   return (
     <div className={`
     flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500 text-white`}>
       <Layout titulo="Cadastro Simples">
+        {visivel === 'tabela' ? 
+        (<>
         <div className="flex justify-end">
-          <Botao cor="green" className="mb-4">Novo Cliente</Botao>
+          <Botao cor="green" className="mb-4" onClick={novoCliente}>
+            Novo Cliente
+          </Botao>
         </div>
-        <Tabela
+        {<Tabela
           clientes={clientes}
           clienteSelecionado={clienteSelecionado}
-          clienteExcluido={clienteExcluido} />
+          clienteExcluido={clienteExcluido} />}
+      </>): 
+      (<Formulario 
+        cliente={cliente}
+        clienteMudou={salvarCliente}
+        cancelado={() => setVisivel('tabela')} 
+        />)
+      }
       </Layout>
 
     </div>
